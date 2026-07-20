a6b7f4461cbc62e7458d875520d9f278ac1c33e6
0x1 <kevin.francart3@gmail.com>
2026-07-20 15:08:14 +0200

    misc

---
+++ tests/runner/test_show.php
@@ -0,0 +1,17 @@
+<?php
+if (!\class_exists('\\PhpursThunks', false)) {
+  class PhpursThunks {
+    public static $thunks = [];
+    public static $cache = [];
+    public static function eval($id) {
+      if (isset(self::$cache[$id]) || \array_key_exists($id, self::$cache)) return self::$cache[$id];
+      if (isset(self::$thunks[$id])) {
+        self::$cache[$id] = self::$thunks[$id]();
+        return self::$cache[$id];
+      }
+      throw new \Exception("FATAL: Unknown thunk " . $id);
+    }
+  }
+}
+require_once __DIR__ . '/output/Main/index.php';
+var_dump(\PhpursThunks::eval('Main_showFFI')("string"));

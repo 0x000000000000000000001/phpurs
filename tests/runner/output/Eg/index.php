2d0b967ecee8b6c7433021d73d51f0e31f2db8b8
0x1 <kevin.francart3@gmail.com>
2026-07-20 14:19:40 +0200

    misc

---
+++ tests/runner/output/Eg/index.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Eg;
+
+// ALL IMPORTS: Eg, Prim
+// TO REQUIRE: Eg
+require_once __DIR__ . '/../Eg/index.php';
+
+if (!class_exists(__NAMESPACE__ . '\\Phpurs_Data0')) {
+  class Phpurs_Data0 { public $tag; public function __construct($t) { $this->tag = $t; } }
+  class Phpurs_Data1 { public $tag; public $value0; public function __construct($t, $value0) { $this->tag = $t; $this->value0 = $value0; } }
+  class Phpurs_Data2 { public $tag; public $value0, $value1; public function __construct($t, $value0, $value1) { $this->tag = $t; $this->value0 = $value0; $this->value1 = $value1; } }
+  class Phpurs_Data3 { public $tag; public $value0, $value1, $value2; public function __construct($t, $value0, $value1, $value2) { $this->tag = $t; $this->value0 = $value0; $this->value1 = $value1; $this->value2 = $value2; } }
+  class Phpurs_Data4 { public $tag; public $value0, $value1, $value2, $value3; public function __construct($t, $value0, $value1, $value2, $value3) { $this->tag = $t; $this->value0 = $value0; $this->value1 = $value1; $this->value2 = $value2; $this->value3 = $value3; } }
+  class Phpurs_Data5 { public $tag; public $value0, $value1, $value2, $value3, $value4; public function __construct($t, $value0, $value1, $value2, $value3, $value4) { $this->tag = $t; $this->value0 = $value0; $this->value1 = $value1; $this->value2 = $value2; $this->value3 = $value3; $this->value4 = $value4; } }
+  class Phpurs_Data6 { public $tag; public $value0, $value1, $value2, $value3, $value4, $value5; public function __construct($t, $value0, $value1, $value2, $value3, $value4, $value5) { $this->tag = $t; $this->value0 = $value0; $this->value1 = $value1; $this->value2 = $value2; $this->value3 = $value3; $this->value4 = $value4; $this->value5 = $value5; } }
+}
+if (!\function_exists(__NAMESPACE__ . '\\phpurs_curry_fallback')) {
+  function phpurs_curry_fallback($fn, $args, $expected) {
+    $missing = $expected - \count($args);
+    if ($missing === 1) {
+      return function($a) use ($fn, $args, $expected) {
+        $num = \func_num_args();
+        if ($num > 1) {
+          $merged = \array_merge($args, \func_get_args());
+          $res = $fn(...\array_slice($merged, 0, $expected));
+          return $res(...\array_slice($merged, $expected));
+        }
+        $args[] = $a;
+        return $fn(...$args);
+      };
+    }
+    if ($missing === 2) {
+      return function($a, $b = null) use ($fn, $args, $expected) {
+        $num = \func_num_args();
+        if ($num === 1) { $args[] = $a; return phpurs_curry_fallback($fn, $args, $expected); }
+        if ($num > 2) {
+          $merged = \array_merge($args, \func_get_args());
+          $res = $fn(...\array_slice($merged, 0, $expected));
+          return $res(...\array_slice($merged, $expected));
+        }
+        $args[] = $a; $args[] = $b;
+        return $fn(...$args);
+      };
+    }
+    if ($missing === 3) {
+      return function($a, $b = null, $c = null) use ($fn, $args, $expected) {
+        $num = \func_num_args();
+        if ($num === 1) { $args[] = $a; return phpurs_curry_fallback($fn, $args, $expected); }
+        if ($num === 2) { $args[] = $a; $args[] = $b; return phpurs_curry_fallback($fn, $args, $expected); }
+        if ($num > 3) {
+          $merged = \array_merge($args, \func_get_args());
+          $res = $fn(...\array_slice($merged, 0, $expected));
+          return $res(...\array_slice($merged, $expected));
+        }
+        $args[] = $a; $args[] = $b; $args[] = $c;
+        return $fn(...$args);
+      };
+    }
+    if ($missing === 4) {
+      return function($a, $b = null, $c = null, $d = null) use ($fn, $args, $expected) {
+        $num = \func_num_args();
+        if ($num === 1) { $args[] = $a; return phpurs_curry_fallback($fn, $args, $expected); }
+        if ($num === 2) { $args[] = $a; $args[] = $b; return phpurs_curry_fallback($fn, $args, $expected); }
+        if ($num === 3) { $args[] = $a; $args[] = $b; $args[] = $c; return phpurs_curry_fallback($fn, $args, $expected); }
+        if ($num > 4) {
+          $merged = \array_merge($args, \func_get_args());
+          $res = $fn(...\array_slice($merged, 0, $expected));
+          return $res(...\array_slice($merged, $expected));
+        }
+        $args[] = $a; $args[] = $b; $args[] = $c; $args[] = $d;
+        return $fn(...$args);
+      };
+    }
+    return function(...$more) use ($fn, $args, $expected) {
+      $merged = \array_merge($args, $more);
+      if (\count($merged) >= $expected) {
+        $res = $fn(...\array_slice($merged, 0, $expected));
+        if (\count($merged) > $expected) {
+          return $res(...\array_slice($merged, $expected));
+        }
+        return $res;
+      }
+      return phpurs_curry_fallback($fn, $merged, $expected);
+    };
+  }
+}
+\PhpursThunks::$thunks['Eg_Bar__prime__'] = function() { $v = (function() {
+  $__fn = function($value0, $value1 = null) use (&$__fn) {
+  $__num = \func_num_args();
+  if ($__num < 2) {
+    return phpurs_curry_fallback($__fn, \func_get_args(), 2);
+  }
+  $__res = new Phpurs_Data2("Bar'", $value0, $value1);
+  goto __end;;
+  __end:
+  return $__num > 2 ? $__res(...\array_slice(\func_get_args(), 2)) : $__res;
+  };
+  return $__fn;
+})(); return $v; };
+$GLOBALS['Prim_undefined'] = function() { throw new \Exception("undefined"); };
+
+
+
--- tests/runner/src/Main.purs
+++ tests/runner/src/Main.purs
@@ -1,39 +1,73 @@
 module Main where
 
 import Prelude
+
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
 import Effect.Console (log)
 import Test.Assert (assertEqual)
+import CustomAssert (assertThrows)
 
-data Maybe a = Nothing | Just a
+force :: forall a b. (Unit -> b) -> b
+force f = f unit
 
-f :: Int -> Int
-f x = case x of
-  0 -> 0
-  n | _ <- n -> f (x - 1)
-  _ -> f (x - 2)
+alpha = { backref: \_ -> bravo, x: 1 }
+bravo = force \_ -> alpha.x
 
-g :: Int -> Int
-g x = case x of
-  0 -> 0
-  n | n == n, true -> g (x - 1)
-  _ -> g (x - 2)
 
-weirdsum :: Int -> (Int -> Maybe Int) -> Int -> Int
-weirdsum accum f n = case n of
-  0 -> accum
-  x | Just y <- f x -> weirdsum (accum + y) f (n - 1)
-  _ -> weirdsum accum f (n - 1)
-
-tricksyinners :: Int -> Int -> Int
-tricksyinners accum x = case x of
-  0 -> accum + f' x * f' x
-  n -> tricksyinners (accum + 2) (n - 1)
+complicatedIdentity :: forall a. a -> a
+complicatedIdentity = h
   where
-  f' y = y + 3
+  -- This highly contrived function tests that escalating force is caught and
+  -- doesn't cause an infinite loop during compilation. ("Escalating force"
+  -- means that invoking `f` with two argument leads to `f` being invoked with
+  -- three arguments, and so on.)
+
+  -- If the escalating loop in `f` isn't taken into account, `h` might be
+  -- initialized before `g`, which will lead to a run-time error. The intended
+  -- behavior is to lazily initialize `g` and `h` together, and let the fact
+  -- that at run time `g` never actually dereferences `h` resolve the
+  -- initialization ordering.
+
+  f :: forall a. Int -> { tick :: a -> a, tock :: a -> a }
+  f n = { tick: if n <= 0 then identity else (f (n - 1)).tock identity, tock: \a -> g n a }
+
+  g :: forall a. Int -> a -> a
+  g = (\bit -> if bit then \n -> (f n).tick else const h) true
+
+  h :: forall a. a -> a
+  h = (\n -> (f n).tick) 10
+
+
+foreign import runtimeImportImpl :: forall a. Maybe String -> (String -> Maybe String) -> String -> (Maybe String -> Effect a) -> Effect a
+
+runtimeImport :: forall a. String -> (Maybe String -> Effect a) -> Effect a
+runtimeImport = runtimeImportImpl Nothing Just
+
+type ID = forall a. a -> a
 
 main = do
-  assertEqual { expected: 0, actual: f 100000 }
-  assertEqual { expected: 0, actual: g 100000 }
-  assertEqual { expected: 20, actual: weirdsum 0 (\x -> if x < 5 then Just (2 * x) else Nothing) 100000 }
-  assertEqual { expected: 200009, actual: tricksyinners 0 100000 }
-  log "Done"
+  err <- assertThrows \_ ->
+    let
+      selfOwn = { a: 1, b: force \_ -> selfOwn.a }
+    in selfOwn
+  assertEqual { actual: err, expected: "ReferenceError: selfOwn was needed before it finished initializing (module Main, line 52)" }
+
+  err2 <- assertThrows \_ ->
+    let
+      f = (\_ -> { left: g identity, right: h identity }) unit
+
+      g :: ID -> ID
+      g x = (j x x x).right
+
+      h :: ID -> ID -> { left :: ID, right :: ID }
+      h x = j x x
+
+      j x y z = { left: x y z, right: f.left }
+    in f
+  assertEqual { actual: err2, expected: "ReferenceError: f was needed before it finished initializing (module Main, line 66)" }
+
+  assertEqual { actual: bravo, expected: 1 }
+  runtimeImport "InitializationError" \err3 -> do
+    assertEqual { actual: err3, expected: Just "ReferenceError: alphaArray was needed before it finished initializing (module InitializationError, line 0)" } -- TODO: fix the 0
+    log "Done"

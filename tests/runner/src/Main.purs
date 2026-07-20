7475f548dfce16d2865c43421768e0cd542e8e23
0x1 <kevin.francart3@gmail.com>
2026-07-20 15:00:50 +0200

    misc

--- tests/runner/src/Main.purs
+++ tests/runner/src/Main.purs
@@ -1,16 +1,22 @@
 module Main where
 
--- Note that Data.Profunctor is not in the dependencies of any types imported
--- here. The package that contains that module must be a dependency of the test
--- project.
-
 import Prelude
 
 import Effect.Console (log)
+import Effect.Uncurried (EffectFn3, mkEffectFn7, runEffectFn3, runEffectFn7)
+import Test.Assert (assert)
 
-data Test a
-  = Test1 ((Array a -> Int) -> Int)
-  | Test2 { f :: ({ a :: a } -> Int) -> Int }
-derive instance Functor Test
+testBothWays = do
+  res <- (runEffectFn7 $ mkEffectFn7 \x1 x2 x3 x4 x5 x6 x7 -> pure 42) 1 2 3 4 5 6 7
+  assert $ res == 42
 
-main = log "Done"
+foreign import add3 :: EffectFn3 String String String String
+
+testRunFn = do
+  str <- runEffectFn3 add3 "a" "b" "c"
+  assert $ str == "abc"
+
+main = do
+  testBothWays
+  testRunFn
+  log "Done"
